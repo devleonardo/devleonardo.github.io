e58f20446eef064f7022be1eceb985e119a83f07
devleonardo <leonardomartins.ads@gmail.com>
2020-12-07 23:13:32 -0300

    update sass

--- projetos/bikcraft/js/main.js
+++ projetos/bikcraft/js/main.js
@@ -1,29 +1,91 @@
-if(window.SimpleSlide){
+$(".rslides").responsiveSlides({
+  auto: true,             // Boolean: Animate automatically, true or false
+  speed: 500,            // Integer: Speed of the transition, in milliseconds
+  timeout: 5000,          // Integer: Time between slide transitions, in milliseconds
+});
 
-    new SimpleSlide({
-        slide: 'quote', // nome do atributo data-slide="principal"
-        time: 5000, // tempo de transição dos slides
-        pauseOnHover: true, // pausa a transição automática
-    });
-    
-    new SimpleSlide({
-        slide: 'portifolio', // nome do atributo data-slide="principal"
-        time: 5000, // tempo de transição dos slides
-        nav: true,
-        pauseOnHover: true, // pausa a transição automática
-    });
+$(".rslides_portfolio").responsiveSlides({
+  auto: true,             // Boolean: Animate automatically, true or false
+  speed: 500,            // Integer: Speed of the transition, in milliseconds
+  timeout: 4000,          // Integer: Time between slide transitions, in milliseconds
+  pager: true,           // Boolean: Show pager, true or false
+});
 
-}
+Visibility.onVisible(function(){
+	setTimeout(function () {
+		$(".introducao h1").addClass("animated fadeInDown");
+	}, 400);
+	setTimeout(function () {
+		$(".introducao blockquote").addClass("animated fadeInDown");
+	}, 800);
+	setTimeout(function () {
+		$(".introducao .btn").addClass("animated fadeInDown");
+	}, 1200);
+	setTimeout(function () {
+		$(".animar").addClass("animated fadeInDown");
+	}, 1600);
 
-if(window.SimpleAnime){
-    new SimpleAnime();
-}
+	setTimeout(function () {
+		$(".introducao-interna h1").addClass("animated fadeInDown");
+	}, 400);
+	setTimeout(function () {
+		$(".introducao-interna p").addClass("animated fadeInDown");
+	}, 800);
+	setTimeout(function () {
+		$(".animar-interno").addClass("animated fadeInDown");
+	}, 1200);
+});
 
-if(window.SimpleForm){
-    new SimpleForm({
-        form: ".formphp",
-        button: "#enviar",
-        erro: "<div id='form-erro'><h2>Erro no envio!</h2><p>Um erro ocorreu, tente para o email contato@bikcraft.com.</p></div>",
-        sucesso: "<div id='form-sucesso'<h2>Formulário enviado com sucesso</h2><p>Em breve eu entro em contato com você.</p></div>"
-    })
-}
+// Formulario
+
+$('.formphp').on('submit', function() {
+	var emailContato = "contato@bikcraft.com"; // Escreva aqui o seu e-mail
+
+	var that = $(this),
+			url = that.attr('action'),
+			type = that.attr('method'),
+			data = {};
+	
+	that.find('[name]').each(function(index, value) {
+		var that = $(this),
+				name = that.attr('name'),
+				value = that.val();
+				
+		data[name] = value;
+	});
+	
+	$.ajax({
+		url: url,
+		type: type,
+		data: data,
+		success: function(response) {
+		
+			if( $('[name="leaveblank"]').val().length != 0 ) {
+				$('.formphp').html("<div id='form-erro'></div>");
+				$('#form-erro').html("<span>Falha no envio!</span><p>Você pode tentar novamente, ou enviar direto para o e-mail " + emailContato + " </p>")
+				.hide()
+				.fadeIn(1500, function() {
+				$('#form-erro');
+				});
+			} else {
+			
+				$('.formphp').html("<div id='form-send'></div>");
+				$('#form-send').html("<span>Mensagem enviada!</span><p>Em breve eu entro em contato com você. Abraços.</p>")
+				.hide()
+				.fadeIn(1500, function() {
+				$('#form-send');
+				});
+			};
+		},
+		error: function(response) {
+			$('.formphp').html("<div id='form-erro'></div>");
+			$('#form-erro').html("<span>Falha no envio!</span><p>Você pode tentar novamente, ou enviar direto para o e-mail " + emailContato + " </p>")
+			.hide()
+			.fadeIn(1500, function() {
+			$('#form-erro');  
+		});
+		}
+	});
+	
+	return false;
+});
