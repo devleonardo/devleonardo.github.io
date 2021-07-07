957053d8033f6aacd67df3aca2c0bd0c19d7faa4
devleonardo <leonardomartins.ads@gmail.com>
2021-07-07 01:07:16 -0300

    update fixed menu

--- js/script.js
+++ js/script.js
@@ -39,3 +39,11 @@
     
 })();
 
+// BACKGROUND MENU FIXO
+(function () {
+  var menu = document.querySelector('.navegador');
+  window.addEventListener('scroll', function () {
+      if (window.scrollY > 0) menu.classList.add('menuFixo');
+      else menu.classList.remove('menuFixo');
+  });
+})();
