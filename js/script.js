44fd15ea9ad724c2c7b3659c0a819de2b5ada51e
devleonardo <leonardomartins.ads@gmail.com>
2021-07-08 02:23:52 -0300

    update mobile

--- js/script.js
+++ js/script.js
@@ -48,4 +48,14 @@
   });
 })();
 
-// CARREGAMENTO DO CONTEÚDO
+// EFEITO MÁQUINA DE ESCREVER
+function typeWriter(elemento) {
+  const textoArray = elemento.innerHTML.split('');
+  elemento.innerHTML = '';
+  textoArray.forEach((letra, i) => {
+    setTimeout(() => elemento.innerHTML += letra, 75 * i);
+  });
+}
+
+const titulo = document.querySelector('.subtitulo h2');
+typeWriter(titulo);
