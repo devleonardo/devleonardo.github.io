24b4af4c84537d13431e1aa60b403114bda1ca58
devleonardo <leonardomartins.ads@gmail.com>
2021-08-17 23:32:58 -0300

    update projetos upsign

--- js/script.js
+++ js/script.js
@@ -69,3 +69,22 @@ function ativarCheckbox() {
 
 const linkMenu = document.querySelector('.barra nav');
 linkMenu.addEventListener("click", ativarCheckbox);
+
+// SLIDE
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";  
+  }
+  x[slideIndex-1].style.display = "block";  
+}
