ad3c6b089227466875524000f90cc0e9cf44a56d
devleonardo <leonardomartins.ads@gmail.com>
2021-08-25 00:35:35 -0300

    adicionado plugin facebook chat

--- js/script.js
+++ js/script.js
@@ -88,3 +88,23 @@ function showDivs(n) {
   }
   x[slideIndex-1].style.display = "block";  
 }
+
+// PLUGIN FACEBOOK
+var chatbox = document.getElementById('fb-customer-chat');
+    chatbox.setAttribute("page_id", "101110651492053");
+    chatbox.setAttribute("attribution", "biz_inbox");
+
+window.fbAsyncInit = function() {
+  FB.init({
+    xfbml            : true,
+    version          : 'v11.0'
+  });
+};
+
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = 'https://connect.facebook.net/pt_BR/sdk/xfbml.customerchat.js';
+  fjs.parentNode.insertBefore(js, fjs); 
+}(document, 'script', 'facebook-jssdk'));
