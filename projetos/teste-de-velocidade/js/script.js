b1c0f79766c4d71de9e146c09857c08114717d91
Leonardo Martins <leonardomartins.ads@gmail.com>
2023-01-11 11:15:58 -0300

    update minha conexão

--- projetos/teste-de-velocidade/js/script.js
+++ projetos/teste-de-velocidade/js/script.js
@@ -6,7 +6,7 @@ minhaconexao = () => {
 for (i = 0; i < 4; i++) {
     (function (i) {
         setTimeout(function () {
-            open('https://www.googleadservices.com/pagead/aclk?sa=L&ai=DChcSEwiomIyPlK_6AhUO05EKHfy9C34YABAAGgJjZQ&ohost=www.google.com&cid=CAESaeD2UBeCojTh00WYnAQemmWoHWIX9UFVKt4Fyw-UQTp6UBDSBXlZQfwa5dP5-aNL3_1TfKjR0arT3lhedAZtU06DU5lnruO90FGeK8TV6qEFG7MO6Ju9Q6LZj9zrCBeWbwfoQCrdU7qDLA&sig=AOD64_1ZGjziQENvp16HJ59cindjs7sG4w&q&adurl&ved=2ahUKEwj80YaPlK_6AhU9rZUCHb4iBwoQ0Qx6BAgEEAE')
+            open('https://www.minhaconexao.com.br/?run')
         }, tempo*i);
     })(i);
 };
